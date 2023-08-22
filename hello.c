c026755b93c4fbeea9db0f7f20063d3491be46fa
ChancellorGlass <cglass6@huskers.unl.edu>
2023-08-22 10:06:51 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Chancellor D. Glass
+ *         cglass6@huskers.unl.edu
+ * Date: 2023/08/22
+ *
+ * A simple C program for Hack 1.0 in UNL CSCE155E
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Chancellor Daniel Glass\n");
+
+  return 0;
+}
